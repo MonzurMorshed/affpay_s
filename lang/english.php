69be4a4c442450591c26fdbfc57c92602e392117
MonzurMorshed <monzur.mysoftheaven@gmail.com>
2021-10-14 00:52:05 +0600

    code push to git

---
+++ lang/english.php
@@ -0,0 +1,4 @@
+<?php 
+	$slider_title = "High <span class='ml3' style='color:yellow !important;'>QUALITY</span><br>AND TRUST.<br>THAT'S WHAT WE<br>STAND FOR!<br>";
+	$read_more = "READ MORE";
+?>
---
+++ lang/russian.php
@@ -0,0 +1,4 @@
+<?php 
+	$slider_title = 'Высокое <span class = "ml3"  style="color:yellow !important;"> КАЧЕСТВО </span> <br>И ДОВЕРЯТЬ. <br>ЭТО МЫ <br>СТАТЬ ЗА! <br>';
+	$read_more = 'ПОДРОБНЕЕ';
+?>
